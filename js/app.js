cac71d0f57233f8698d98b0b989dfde6d005ca29
gaboesquivel <contact@gaboesquivel.com>
2018-05-27 18:25:15 -0600

    Update 2018-05-28T00:25:11.464Z

--- js/app.js
+++ js/app.js
@@ -156,37 +156,6 @@ $(function() {
 		$el.addClass('visible');
 	}, 1000);
 })
-//ResetForm validation
-$(function() {
-	if (!$('#reset-form').length) {
-        return false;
-    }
-
-    var resetValidationSettings = {
-	    rules: {
-	        email1: {
-	            required: true,
-	            email: true
-	        }
-	    },
-	    messages: {
-	        email1: {
-	            required: "Please enter email address",
-	            email: "Please enter a valid email address"
-	        }
-	    },
-	    invalidHandler: function() {
-			animate({
-				name: 'shake',
-				selector: '.auth-container > .card'
-			});
-		}
-	}
-
-	$.extend(resetValidationSettings, config.validations);
-
-    $('#reset-form').validate(resetValidationSettings);
-})
 //LoginForm validation
 $(function() {
 	if (!$('#login-form').length) {
@@ -221,6 +190,37 @@ $(function() {
 	$.extend(loginValidationSettings, config.validations);
 
     $('#login-form').validate(loginValidationSettings);
+})
+//ResetForm validation
+$(function() {
+	if (!$('#reset-form').length) {
+        return false;
+    }
+
+    var resetValidationSettings = {
+	    rules: {
+	        email1: {
+	            required: true,
+	            email: true
+	        }
+	    },
+	    messages: {
+	        email1: {
+	            required: "Please enter email address",
+	            email: "Please enter a valid email address"
+	        }
+	    },
+	    invalidHandler: function() {
+			animate({
+				name: 'shake',
+				selector: '.auth-container > .card'
+			});
+		}
+	}
+
+	$.extend(resetValidationSettings, config.validations);
+
+    $('#reset-form').validate(resetValidationSettings);
 })
 //SignupForm validation
 $(function() {
